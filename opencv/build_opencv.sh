493351789b26131369790a5a4dd789b502e0e9d1
weiyi.li <weiyi.li@vista.co>
2021-01-26 09:19:21 +1300

    updated build_opencv.sh

--- opencv/build_opencv.sh
+++ opencv/build_opencv.sh
@@ -4,16 +4,16 @@ cd `dirname $0`
 WD=`pwd`
 
 CV_SUBMODULE_DIR="$WD/../ThirdParty/opencv"
-CV_VERSION="2.4.13"
+CV_VERSION="2.4.13.6"
 
 if [ -d "$CV_SUBMODULE_DIR" ] ; then
 	cd $CV_SUBMODULE_DIR
-	
+
 	CV_VERSION=`git describe --always --dirty --tags`
 	CV_SRC=`pwd`
-	
+
 	cd $WD
-	
+
 	rm -rf "*-dirty"
 else
 	unset CV_SUBMODULE_DIR
@@ -26,32 +26,32 @@ ANDROID_CMAKE_FILE="android.toolchain.cmake"
 
 if [ ! "$CV_SRC" ] ; then
 	CV_SRC="$WD/$CV_NAME"
-	
+
 	CV_ZIP="$CV_NAME.zip"
 	CV_URL="http://downloads.sourceforge.net/project/opencvlibrary/opencv-unix/$CV_VERSION/$CV_ZIP"
 
 	# standard on MacOSX
 	DOWNLOAD_DIR="$HOME/Downloads"
 
-	if [ ! -d "$CV_SRC" ] ; then
-	    if [ ! -f "$CV_ZIP" ] && [ -f "$DOWNLOAD_DIR/$CV_ZIP" ] ; then
-	        echo "found $DOWNLOAD_DIR/$CV_ZIP"
-	        CV_ZIP="$DOWNLOAD_DIR/$CV_ZIP"
-	    fi
-	    if [ ! -f "$CV_ZIP" ] ; then
-	        echo "didn't find `basename $CV_ZIP` in $PWD or $DOWNLOAD_DIR"
-	        echo "downloading OpenCV from $CV_URL"
-        
-	        wget $CV_URL || exit -1
-	    fi
-    
-	    echo "unzipping $CV_ZIP"
-	    unzip $CV_ZIP || exit -1
-	fi
+	# if [ ! -d "$CV_SRC" ] ; then
+	#     if [ ! -f "$CV_ZIP" ] && [ -f "$DOWNLOAD_DIR/$CV_ZIP" ] ; then
+	#         echo "found $DOWNLOAD_DIR/$CV_ZIP"
+	#         CV_ZIP="$DOWNLOAD_DIR/$CV_ZIP"
+	#     fi
+	#     if [ ! -f "$CV_ZIP" ] ; then
+	#         echo "didn't find `basename $CV_ZIP` in $PWD or $DOWNLOAD_DIR"
+	#         echo "downloading OpenCV from $CV_URL"
+
+	#         wget $CV_URL || exit -1
+	#     fi
+
+	#     echo "unzipping $CV_ZIP"
+	#     unzip $CV_ZIP || exit -1
+	# fi
 fi
 
-ANDROID_CMAKE_URL="https://raw.githubusercontent.com/taka-no-me/android-cmake/master/android.toolchain.cmake"
-wget -q $ANDROID_CMAKE_URL -O $ANDROID_CMAKE_FILE || exit -1
+# ANDROID_CMAKE_URL="https://raw.githubusercontent.com/taka-no-me/android-cmake/master/android.toolchain.cmake"
+# wget -q $ANDROID_CMAKE_URL -O $ANDROID_CMAKE_FILE || exit -1
 
 
 
@@ -59,7 +59,8 @@ for ARCH in "armeabi-v7a" "x86" "arm64-v8a" "x86_64"
 do
 	echo "---- building $ARCH ----"
 	BUILD_DIR="$WD/build-$CV_VERSION-"$ARCH
-	DEST_DIR="$WD/../card.io/src/main/jni/lib/"$ARCH
+	# DEST_DIR="$WD/../card.io/src/main/jni/lib/"$ARCH
+	DEST_DIR="$WD/output/"$ARCH
 
 	mkdir -p $BUILD_DIR
 	cd $BUILD_DIR
